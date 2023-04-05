4104bb67dff7463e1329e0838f039956000e6af4
khang-nguyen2907 <giakhang29079@gmail.com>
2023-04-05 14:53:54 +0700

    fix: relative path

--- t5x/scripts/set_up_colab.sh
+++ t5x/scripts/set_up_colab.sh
@@ -1,6 +1,6 @@
-pip install -r t5x/requirements.txt
+pip install -r setup/t5x/requirements.txt
 pip install -U jax jaxlib flax
-git clone --branch=main https://github.com/google-research/t5x
+git clone --branch=main https://github.com/khang-nguyen2907/t5x.git
 cd t5x
 python3 -m pip install -e . -f \
   https://storage.googleapis.com/jax-releases/libtpu_releases.html
